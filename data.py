6f95e899c8826920e97f11cdfb1eb5c0e2199f67
Jérôme <git@jeromecst.com>
2021-03-28 10:52:23 +0200

    data

--- data.py
+++ data.py
@@ -5,6 +5,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 plt.style.use(['seaborn-bright','classic'])
 
-data = pd.read_csv('CrimesHalf.csv', low_memory=False)
-print(data.head(100))
-print(data.columns)
+data = pd.read_csv('Crimes1M.csv', low_memory=False)
+del data['ID']
+del data['Case Number']
+del data['Updated On']
+del data['Longitude']
+del data['Latitude']
+del data['Location']
+del data['X Coordinate']
+del data['Y Coordinate']
+data = data.to_numpy() 
+print("shape :",data.shape)
+print("dates :", data[10:30,0])
+
+def split_date(X):
+    date = data[:,0]
+    return 
+
+split_date(data)
