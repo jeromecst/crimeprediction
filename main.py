d77f85c39faa607f5cd3a176c7685f83564ca72e
jerome <jexowa@gmail.com>
2021-04-04 12:26:21 +0200

    option pour ne pas faire le preprocessing à chaque fois

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import numpy as np
 import preprocessing
 import train
+import os.path
+from os import path
+
+file = "Crimes100K"
 
 def affichage_encodage():
     for i in range(len(prepro.features_description)):
         for j in range(2):
             print("encodage du",prepro.features_description[i],j,prepro.encodage[i][j])
 
-prepro = preprocessing.preprocessing("Crimes100K.csv")
-prepro.ajout_dates()
-prepro.Kmeans_coordinate(50)
-prepro.ajout_Kmeans_coordinate()
-prepro.affiche_Kmeans_coordinate()
-data_encodée = prepro.encodage_features()
+ignorePreprocessedFile = False
+if path.isfile(f"{file}_prepro.csv") and not ignorePreprocessedFile:
+    print("file found!")
+    prepro = preprocessing.preprocessing(file, already_preprocessed=True)
+    data_encodée = prepro.data
+else:
+    prepro = preprocessing.preprocessing(file)
+    prepro.ajout_dates()
+    prepro.Kmeans_coordinate(50)
+    prepro.ajout_Kmeans_coordinate()
+    #prepro.affiche_Kmeans_coordinate()
+    data_encodée = prepro.encodage_features()
+    prepro.save_to_csv(file)
+
 X, Y = prepro.XYsplit(data_encodée)
+print(X.shape, Y.shape)
