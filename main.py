efcb5ab75b34c58045285d02e4b6d17bc6958295
mathis.dupont <mathis.dupont@u-psud.fr>
2021-04-04 11:58:03 +0200

    Ajout de l'entrainement

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import numpy as np
 import preprocessing
 import train
+import time
 
 def affichage_encodage():
     for i in range(len(prepro.features_description)):
         for j in range(2):
             print("encodage du",prepro.features_description[i],j,prepro.encodage[i][j])
 
+print("\n----------------------Début du preprocessing-----------------------\n") 
+time_before = time.time()
 prepro = preprocessing.preprocessing("Crimes100K.csv")
 prepro.ajout_dates()
 prepro.Kmeans_coordinate(50)
 prepro.ajout_Kmeans_coordinate()
-prepro.affiche_Kmeans_coordinate()
+time_after = time.time()
+print("Temps pour le preprocessing : ", (time_after-time_before), " secondes\n")
+#prepro.affiche_Kmeans_coordinate()
 data_encodée = prepro.encodage_features()
 X, Y = prepro.XYsplit(data_encodée)
+print("----------------------Début du training-----------------------\n")      
+train = train.train(X,Y)
+train.traintestsplit(0.3)
+print("\n----------Début du training_GaussienNB------------\n")      
+train.fit_GaussNB()
+print("\n----------Début du training_DecisionTree----------\n")
+train.fit_DecisionTree()
+
+
+
--- train.py
+++ train.py
@@ -1,21 +1,44 @@
 import numpy as np
 import sklearn
 
+from sklearn.naive_bayes import GaussianNB
+from sklearn import tree
+
 class train:
     def __init__(self, X, Y):
         self.X = X
         self.Y = Y
-        self.N = X.shape(0)
-        self.D = X.shape(1)
-        self.X_train, self.X_test, self.Y_train, self.Y_test
+        self.N = X.shape[0]
+        self.D = X.shape[1]
+        self.X_train = 0
+        self.X_test = 0
+        self.Y_train = 0
+        self.Y_test = 0
 
     def load_data(self, X, Y):
         self.X = X
         self.Y = Y
-        self.N = X.shape(0)
-        self.D = X.shape(1)
+        self.N = X.shape[0]
+        self.D = X.shape[1]
 
     def traintestsplit(self, test_ratio=.3):
         self.X_train, self.X_test, self.Y_train, self.Y_test = \
-                sklearn.model_selection.train_test_split(X, Y, test_size=test_ratio)
+                sklearn.model_selection.train_test_split(self.X, self.Y, test_size=test_ratio)
 
+    def fit_GaussNB(self):
+        '''
+        Entraînement de notre modèle
+        '''
+        gnb = GaussianNB()
+        gnb.fit(self.X_train, self.Y_train)
+        y_pred = gnb.predict(self.X_test)
+        print("Ratio de bien placé pour le Bayessien naif = ", ((self.Y_test == y_pred).sum() / self.X_test.shape[0]))
+
+        
+        
+    def fit_DecisionTree(self):
+        clf = tree.DecisionTreeClassifier()
+        clf = clf.fit(self.X_train, self.Y_train)
+        y_pred = clf.predict(self.X_test)
+        print("Ratio de bien placé pour le Decision tree = ", ((self.Y_test == y_pred).sum() / self.X_test.shape[0]))
+        
