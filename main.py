a78ea5b44487a2ce04a325d847f1fa40043e9032
jerome <jexowa@gmail.com>
2021-04-04 20:26:43 +0200

    find best parameters for DecisionTree

--- main.py
+++ main.py
@@ -4,9 +4,50 @@ import preprocessing
 import train
 import time
 import os.path
+import matplotlib.pyplot as plt
 from os import path
 
-file = "Crimes100K"
+file = "Crimes100KEq" # ne pas écrire .csv
+
+def bestParamDecisionTree(train, X, Y):
+    """
+    Peut prendre beaucoup de temps, à éxécuter seulement sur un petit
+    ensemble de données ( < 100K )
+    """
+    fig, ax = plt.subplots(2, 2, figsize=(15, 10))
+    score = []
+    sample_list = range(2, 300, 50)
+    for min_sample_split in sample_list:
+        score += [train.fit_DecisionTree(min_samples_split = min_sample_split)]
+    ax[0][0].plot(sample_list, score)
+    ax[0][0].set_xlabel("min_samples_split")
+    ax[0][0].set_ylabel("score")
+
+    score = []
+    sample_list = range(1, 300, 50)
+    for min_samples_leaf in sample_list:
+        score += [train.fit_DecisionTree(min_samples_leaf = min_samples_leaf)]
+    ax[1][0].plot(sample_list, score)
+    ax[1][0].set_xlabel("min_samples_leaf")
+    ax[1][0].set_ylabel("score")
+
+    score = []
+    feature_list = range(1, 18)
+    for max_features in feature_list:
+        score += [train.fit_DecisionTree(max_features = max_features)]
+    ax[0][1].plot(feature_list, score)
+    ax[0][1].set_xlabel("max_feature")
+    ax[0][1].set_ylabel("score")
+
+    score = []
+    impurity = np.linspace(0, .2, 40)
+    for im in impurity:
+        score += [train.fit_DecisionTree(min_impurity_decrease = im)]
+    ax[1][1].plot(impurity, score)
+    ax[1][1].set_xlabel("min_impurity_decrease")
+    ax[1][1].set_ylabel("score")
+
+    plt.savefig("bestParamDecisionTree")
 
 def affichage_encodage():
     for i in range(len(prepro.features_description)):
@@ -39,11 +80,10 @@ def comparaison_features():
     panda_Tree = pd.DataFrame(compar_date_lieu_Tree, columns=lieux, index=dates)
     print(panda_Tree)
 
-
 ignorePreprocessedFile = False
 print("\n----------------------Début du preprocessing-----------------------\n") 
 if path.isfile(f"{file}_prepro.csv") and not ignorePreprocessedFile:
-    print(f"file found, reusing {file}_prepro.csv !")
+    print(f"file found, re-using {file}_prepro.csv !")
     prepro = preprocessing.preprocessing(file, already_preprocessed=True)
     data_encodée = prepro.data
 else:
@@ -60,8 +100,11 @@ else:
     print("Temps pour le preprocessing : ", (time_after-time_before), " secondes\n")
 
 X, Y = prepro.XYsplit(data_encodée)
-
 train = train.train(X,Y)
+train.traintestsplit(0.3)
+
+bestParamDecisionTree(train, X, Y)
+
 train.traintestsplit(0.3)
 display = True
 print("\n----------Début du training_GaussienNB------------\n")      
